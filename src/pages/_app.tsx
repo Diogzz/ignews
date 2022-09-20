a8dfa5b1f4fa5c2f802a827a982f71551145a8bb
Diogo Cesar <d_cesarrodrigues@hotmail.com>
2022-09-20 12:22:03 -0300

    Adicionando autenticação com Next-Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from 'next/app'
+import { Session } from "next-auth";
+import { SessionProvider  as NextAuthProvider } from "next-auth/react";
+
 import { Header } from '../components/Header';
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{session: Session}>) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
